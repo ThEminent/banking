2d3689644f9a7ff9c0755e7c8f069cb38e70903d
ThEminent <lmukorode@gmail.com>
2024-09-22 17:16:11 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ export default withSentryConfig(nextConfig, {
 
 // Suppresses source map uploading logs during build
 silent: true,
-org: "em",
+org: "em-2j",
 project: "em-banking",
 }, {
 // For all available options, see:
